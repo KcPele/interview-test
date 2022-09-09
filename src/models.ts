1307105a5d51e732f232c9de0a8555cce1e48e12
KcPele <fidekg123@gmail.com>
2022-09-09 10:54:08 +0100

    added title

--- src/models.ts
+++ src/models.ts
@@ -16,36 +16,6 @@ export interface IProps {
     
 }
 
-export interface IData {
-    graph_data: IGraphData,
-    top_locations: {
-        source: string;
-        count: number;
-        percent: number;
-    }[],
-    top_sources: {
-        country: string;
-        count: number;
-        percent: number;
-    }[]
-
-}
 
 
-export interface  IGraphData {
-    views: {
-        string: number;
-    };
-};
 
-
-export interface  ITopSources {
-    source: string;
-    count: number;
-    percent: number;
-}[]
-export interface  ITopLocations{
-    country: string;
-    count: number;
-    percent: number;
-}[]
